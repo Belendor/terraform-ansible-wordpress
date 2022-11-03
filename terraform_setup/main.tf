e89fb3e04b1a40370d771345c252e61f4d5ee731
belendor <dwarf11@gmail.com>
2022-11-03 10:07:06 +0200

    init

---
+++ terraform_setup/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
+
+  name = "main-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = []
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+}
+
+module "app_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "3.17.0"
+
+  name        = "web-sg"
+  description = "Security group for web-servers with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "host" {
+  count = var.ec2_count
+
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
+  vpc_security_group_ids = [module.app_security_group.this_security_group_id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              echo "Hello, World $(hostname -f)" > index.html
+              python3 -m http.server 80 &
+              EOF
+
+  tags = {
+    Name = "Ansible-hostt"
+  }
+}
